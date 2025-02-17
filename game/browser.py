97a9b7ff7de40198bc3214afa567b706fe84c17f
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2025-02-17 22:37:22 +0800

    chore: use the start page

--- game/browser.py
+++ game/browser.py
@@ -116,7 +116,7 @@ class GameBrowser:
             )
 
             try:
-                self.page = self.context.new_page()
+                self.page = self.context.pages[0] if self.context.pages else self.context.new_page()
                 self.page.goto(url)
             except Exception as e:
                 LOGGER.error('Error opening page. Check if certificate is installed. \n%s',e)
